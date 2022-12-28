a07b5302799b463120edeb5d25eb7ca41666b5b6
Jared Zook <jaredzook@gmail.com>
2022-12-27 17:28:10 -0800

    add AppShell

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,31 @@
-import { MantineProvider, Text } from '@mantine/core';
-import Shell from './components/Shell';
+import { AppShell, Header, MantineProvider, Navbar } from "@mantine/core";
 
 export default function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Shell />
+      <AppShell
+        padding="md"
+        navbar={
+          <Navbar width={{ base: 300 }} p="xs">
+            Navbar
+          </Navbar>
+        }
+        header={
+          <Header height={60} p="xs">
+            Header
+          </Header>
+        }
+        styles={(theme) => ({
+          main: {
+            backgroundColor:
+              theme.colorScheme === "dark"
+                ? theme.colors.dark[8]
+                : theme.colors.gray[0],
+          },
+        })}
+      >
+        Application
+      </AppShell>
     </MantineProvider>
   );
 }
