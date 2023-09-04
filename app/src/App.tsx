8234f72936dfdb4bddf19e53b3fc6fa21f4e6c34
Jared Zook <7940096+jtzook@users.noreply.github.com>
2023-09-04 14:40:34 -0700

    update app shell styles

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,35 +1,41 @@
-import { AppShell, Header, MantineProvider, Navbar } from "@mantine/core";
+import {
+  AppShell,
+  Footer,
+  Header,
+  MantineProvider,
+  Navbar,
+} from '@mantine/core'
 
 export default function App() {
   return (
     <MantineProvider
-      theme={{ colorScheme: "dark" }}
+      theme={{ colorScheme: 'dark' }}
       withGlobalStyles
       withNormalizeCSS
     >
       <AppShell
-        padding="md"
-        navbar={
-          <Navbar width={{ base: 300 }} p="xs">
-            Navbar
-          </Navbar>
-        }
+        padding='md'
         header={
-          <Header height={60} p="xs">
+          <Header height={80} p='xs'>
             Header
           </Header>
+        }
+        footer={
+          <Footer height={80} p='xs'>
+            Footer
+          </Footer>
         }
         styles={(theme) => ({
           main: {
             backgroundColor:
-              theme.colorScheme === "dark"
+              theme.colorScheme === 'dark'
                 ? theme.colors.dark[8]
                 : theme.colors.gray[0],
           },
         })}
       >
-        Application
+        Home Page
       </AppShell>
     </MantineProvider>
-  );
+  )
 }
