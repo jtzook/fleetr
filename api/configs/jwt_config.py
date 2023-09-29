387aa2424c475b2091a335f1d1b190b5d3121692
Jared Zook <7940096+jtzook@users.noreply.github.com>
2023-09-29 12:39:15 -0700

    refactor

--- api/configs/jwt_config.py
+++ api/configs/jwt_config.py
@@ -1,9 +1,5 @@
-from flask_jwt_extended import (
-    JWTManager,
-    decode_token,
-)
+from flask_jwt_extended import JWTManager, decode_token
 from flask import request, current_app, jsonify
-
 from database.db_client import check_for_revoked_tokens
 
 
@@ -11,40 +7,32 @@ def init_jwt(app):
     JWTManager(app)
 
     def verify_protected_routes():
-        print("verifying protected routes")
         if request.path.startswith("/api"):
             access_token = request.cookies.get("access_token")
             refresh_token = request.cookies.get("refresh_token")
             csrf_token = request.cookies.get("csrf_token")
 
+            tokens = {
+                "access_token": access_token,
+                "refresh_token": refresh_token,
+                "csrf_token": csrf_token,
+            }
+
             try:
-                for token_name, token_value in {
-                    "access_token": access_token,
-                    "refresh_token": refresh_token,
-                    "csrf_token": csrf_token,
-                }.items():
-                    print(f"checking {token_name}", f" token_value: {token_value}")
+                for token_name, token_value in tokens.items():
                     if token_value:
                         decoded_token = decode_token(token_value)
                         token_jti = decoded_token["jti"]
-                        print(f"checking for revoked {token_name}", token_jti)
                         is_revoked, _ = check_for_revoked_tokens(current_app, token_jti)
-                        print(f"{token_name} is_revoked", is_revoked)
                         if is_revoked:
                             return (
                                 jsonify({"msg": f"{token_name} has been revoked"}),
                                 401,
                             )
-                        return
                 else:
-                    return (
-                        jsonify({"msg": "No tokens present in the request"}),
-                        401,
-                    )
-            except Exception as e:
-                print(f"JWT verification failed: {e}")
+                    return jsonify({"msg": "No tokens present in the request"}), 401
+
+            except Exception:
                 return jsonify({"msg": "JWT verification failed"}), 401
-        else:
-            print("request.path", request.path)
 
     app.before_request(verify_protected_routes)
