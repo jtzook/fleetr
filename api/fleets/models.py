ea76ae8f6a7e795a9e2c7c6ce9d6e0984c04d40b
Jared Zook <7940096+jtzook@users.noreply.github.com>
2024-01-07 08:56:30 -0800

    add fleets django app

--- api/fleets/models.py
+++ api/fleets/models.py
@@ -1,3 +1,77 @@
 from django.db import models
 
-# Create your models here.
+
+class Fleet(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    text = models.CharField(max_length=1000, blank=True)
+    parent = models.ForeignKey(
+        "self", related_name="replies", on_delete=models.SET_NULL, null=True, blank=True
+    )
+    is_deleted = models.BooleanField(default=False)
+    fleet_type = models.ForeignKey(
+        "FleetType",
+        related_name="fleets",
+        on_delete=models.PROTECT,
+    )
+    labels = models.ManyToManyField(
+        "FleetLabel",
+        related_name="fleets",
+        blank=True,
+    )
+    score = models.IntegerField(default=0)
+    private = models.BooleanField(default=False)
+    quote_fleet = models.ForeignKey(
+        "self",
+        related_name="quoted_by",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+    threads = models.ManyToManyField("Thread", related_name="fleets", blank=True)
+    notes = models.CharField(max_length=1000, blank=True)
+    metadata = models.JSONField(default=dict, blank=True)
+
+    class Meta:
+        indexes = [
+            models.Index(
+                fields=["created", "updated"]
+            ),  # Indexing for improved query performance
+        ]
+
+
+class FleetLabel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    text = models.CharField(max_length=100, blank=True)
+    notes = models.CharField(max_length=1000, blank=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["created", "updated"]),
+        ]
+
+
+class FleetType(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    text = models.CharField(max_length=100, blank=True)
+    notes = models.CharField(max_length=1000, blank=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["created", "updated"]),
+        ]
+
+
+class Thread(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    original_post = models.OneToOneField(
+        Fleet, on_delete=models.CASCADE, related_name="thread", null=True
+    )
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["created"]),
+        ]
