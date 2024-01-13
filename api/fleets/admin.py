7e18a57433864fcdc7ec05337c79522547566fef
Jared Zook <7940096+jtzook@users.noreply.github.com>
2024-01-13 12:39:18 -0800

    register fleet models

--- api/fleets/admin.py
+++ api/fleets/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Fleet, FleetLabel, FleetTag, FleetThread
+
+admin.site.register(Fleet)
+admin.site.register(FleetLabel)
+admin.site.register(FleetTag)
+admin.site.register(FleetThread)
