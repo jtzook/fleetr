1875f810a6b89a7c7e0847a26ca71a66f0540bc5
Jared Zook <7940096+jtzook@users.noreply.github.com>
2024-01-14 11:17:45 -0800

    add users/ routes

--- api/api/urls.py
+++ api/api/urls.py
@@ -17,7 +17,22 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+"""
+The following URLs are built in to Django's auth app:
+
+    users/login/ [name='login']
+    users/logout/ [name='logout']
+    users/password_change/ [name='password_change']
+    users/password_change/done/ [name='password_change_done']
+    users/password_reset/ [name='password_reset']
+    users/password_reset/done/ [name='password_reset_done']
+    users/reset/<uidb64>/<token>/ [name='password_reset_confirm']
+    users/reset/done/ [name='password_reset_complete']
+
+"""
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("users/", include("django.contrib.auth.urls")),
     path("fleets/", include("fleets.urls")),
 ]
