78170107ad6ca63861856d7ffe820f925ed99bbd
Jared Zook <7940096+jtzook@users.noreply.github.com>
2024-01-27 11:23:11 -0800

    update index route copy

--- api/api/views.py
+++ api/api/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the fleets index.")
+    return HttpResponse("Hello, world. You're at the api index.")
