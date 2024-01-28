780dc7aa932acb643fa823bd4ff2a529b3de1049
Jared Zook <7940096+jtzook@users.noreply.github.com>
2024-01-28 11:13:47 -0800

    add Separator

--- web/app.vue
+++ web/app.vue
@@ -3,23 +3,24 @@
     <Title>fleets</Title>
   </Head>
 
-  <main class="app dark" :style="appStyles">
-    <!-- <NuxtLoadingIndicator /> -->
-    <NuxtPage />
-  </main>
+  <div class="app dark">
+    <header>
+      <div>
+        <div class="m-4">
+          <NuxtLink to="/">fleetr</NuxtLink>
+        </div>
+        <Separator class="" />
+      </div>
+    </header>
+    <main>
+      <NuxtLoadingIndicator />
+      <NuxtPage />
+    </main>
+  </div>
 </template>
 
 <script setup lang="ts">
-const {
-  theme: {
-    colors: { background: backgroundColor, textPrimary: color },
-  },
-} = useAppConfig()
-
-const appStyles = {
-  // backgroundColor,
-  // color,
-}
+import { Separator } from '@/components/ui/separator'
 </script>
 
 <style>
@@ -31,6 +32,24 @@ const appStyles = {
 .page-leave-to {
   opacity: 0;
   filter: blur(1rem);
+}
+
+header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+header > div {
+  width: 500px;
+  max-width: 80%;
+  /* max-width: 64rem; */
+  padding: 0 1rem;
 }
 </style>
 ```
