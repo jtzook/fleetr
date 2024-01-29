c53ee7addff6aa4a3966bf34d286f269b3af1407
Jared Zook <7940096+jtzook@users.noreply.github.com>
2024-01-28 17:01:28 -0800

    adjust brigthness of theme colors

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -4,10 +4,10 @@
       <div class="flex flex-col items-center space-y-2">
         <div class="text-lg font-bold leading-none">Logo</div>
       </div>
-      <div class="flex h-5 items-center justify-center space-x-4 text-sm">
-        <div class="text-gray-500 w-20 flex justify-end">register</div>
+      <div class="text- flex h-5 items-center justify-center space-x-4 text-lg">
+        <div class="w-20 flex justify-end">register</div>
         <Separator orientation="vertical" />
-        <div class="text-gray-500 w-20 justify-end">login</div>
+        <div class="w-20 justify-end">login</div>
       </div>
     </div>
   </NuxtLink>
