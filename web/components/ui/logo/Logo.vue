2567b2a333c38e780c4ec34b90e2a68c64156f22
Jared Zook <7940096+jtzook@users.noreply.github.com>
2024-02-04 10:49:28 -0800

    Logo

---
+++ web/components/ui/logo/Logo.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1 class="logo">fleetr</h1>
+</template>
+
+<style>
+.logo {
+  font-family: 'Helvetica Neue', sans-serif;
+  font-weight: 900;
+  font-size: 4rem;
+  color: hsl(138, 30%, 40%);
+  text-shadow: 0.25rem 0.25rem 0rem hsl(138, 30%, 50%);
+  user-select: none; /* Prevent text from being highlightable */
+  -webkit-user-select: none; /* Safari specific */
+  -moz-user-select: none; /* Firefox specific */
+  -ms-user-select: none; /* IE specific */
+}
+</style>
--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,21 +1,18 @@
 <template>
-  <NuxtLink to="/register">
-    <div class="p-4 space-y-4">
-      <div class="flex flex-col items-center space-y-2">
-        <div class="text-lg font-bold leading-none">Logo</div>
-      </div>
-      <div
-        class="flex h-14 space-x-4 items-center justify-center space-x-4 text-lg"
-      >
+  <div class="flex flex-col items-center space-y-2">
+    <Logo />
+    <div class="flex h-14 space-x-4 items-center justify-center text-lg">
+      <NuxtLink to="/register">
         <Button class="w-20">register</Button>
-        <Separator orientation="vertical" />
-        <Button class="w-20">login</Button>
-      </div>
+      </NuxtLink>
+      <Separator orientation="vertical" />
+      <Button class="w-20">login</Button>
     </div>
-  </NuxtLink>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { Separator } from '~/components/ui/separator'
 import { Button } from '~/components/ui/button'
+import Logo from '~/components/ui/logo/Logo.vue'
 </script>
