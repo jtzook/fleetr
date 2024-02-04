51d5e5f1a7f4a14543ab9d298332a544b4aeb694
Jared Zook <7940096+jtzook@users.noreply.github.com>
2024-02-04 11:00:11 -0800

    more logo styling

--- web/components/ui/logo/Logo.vue
+++ web/components/ui/logo/Logo.vue
@@ -1,14 +1,14 @@
 <template>
-  <h1 class="logo">fleetr</h1>
+  <h1 class="logo mb-1">fleetr</h1>
 </template>
 
 <style>
 .logo {
   font-family: 'Helvetica Neue', sans-serif;
   font-weight: 900;
-  font-size: 4rem;
+  font-size: 5rem;
   color: hsl(138, 30%, 40%);
-  text-shadow: 0.25rem 0.25rem 0rem hsl(138, 30%, 50%);
+  text-shadow: 0rem 0.15rem 0.5rem hsl(138, 30%, 50%);
   user-select: none; /* Prevent text from being highlightable */
   -webkit-user-select: none; /* Safari specific */
   -moz-user-select: none; /* Firefox specific */
